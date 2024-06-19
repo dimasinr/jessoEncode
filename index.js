d9c0d61dc0f6a535c708d1802f3c1c2b919337a2
dimas indra <dimas.indra773@gmail.com>
2024-06-19 13:59:48 +0700

    update version

--- index.js
+++ index.js
@@ -10,16 +10,26 @@ function base64UrlEncode(str) {
 }
 
 function base64UrlDecode(str) {
-    str = str.replace(/-/g, '+').replace(/_/g, '/');
-    const padding = 4 - (str.length % 4);
-    if (padding !== 4) {
-      str += '='.repeat(padding);
-    }
-    return Buffer.from(str, 'base64').toString();
+  str = str.replace(/-/g, "+").replace(/_/g, "/");
+  const padding = 4 - (str.length % 4);
+  if (padding !== 4) {
+    str += "=".repeat(padding);
+  }
+  return Buffer.from(str, "base64").toString();
 }
-  
+
 function base64UrlEncodeJSON(obj) {
   return base64UrlEncode(JSON.stringify(obj));
+}
+
+function verify(str, signature, secret) {
+  const expectedSignature = sign(str, secret);
+  return signature === expectedSignature;
+}
+
+function sign(str, secret) {
+  const hash = CryptoJS.HmacSHA256(str, secret);
+  return base64UrlEncode(CryptoJS.enc.Base64.stringify(hash));
 }
 
 function jessEncode(payload, secret) {
@@ -37,21 +47,19 @@ function jessEncode(payload, secret) {
 }
 
 function jessDecode(token, secret) {
-    const [encodedHeader, encodedPayload, signature] = token.split('.');
-    const data = JSON.parse(base64UrlDecode(encodedPayload));
-  
-    const validSignature = verify(`${encodedHeader}.${encodedPayload}`, signature, secret);
-    if (!validSignature) {
-      throw new Error('Invalid signature');
-    }
-  
-    return {data};
-  }
-  
+  const [encodedHeader, encodedPayload, signature] = token.split(".");
+  const data = JSON.parse(base64UrlDecode(encodedPayload));
 
-function sign(str, secret) {
-  const hash = CryptoJS.HmacSHA256(str, secret);
-  return base64UrlEncode(CryptoJS.enc.Base64.stringify(hash));
+  const validSignature = verify(
+    `${encodedHeader}.${encodedPayload}`,
+    signature,
+    secret
+  );
+  if (!validSignature) {
+    throw new Error("Invalid signature");
+  }
+
+  return { data };
 }
 
 module.exports = {
