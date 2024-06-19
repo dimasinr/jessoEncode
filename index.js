6b07a4131c90e7798b979a5e3ad55d01990bfcfa
dimas indra <dimas.indra773@gmail.com>
2024-06-19 13:42:34 +0700

    update jesso

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ function base64UrlEncode(str) {
     .replace(/=+$/, "");
 }
 
+function base64UrlDecode(str) {
+    str = str.replace(/-/g, '+').replace(/_/g, '/');
+    const padding = 4 - (str.length % 4);
+    if (padding !== 4) {
+      str += '='.repeat(padding);
+    }
+    return Buffer.from(str, 'base64').toString();
+}
+  
 function base64UrlEncodeJSON(obj) {
   return base64UrlEncode(JSON.stringify(obj));
 }
@@ -27,6 +36,19 @@ function jessEncode(payload, secret) {
   return `${token}.${signature}`;
 }
 
+function jessDecode(token, secret) {
+    const [encodedHeader, encodedPayload, signature] = token.split('.');
+    const header = JSON.parse(base64UrlDecode(encodedHeader));
+    const payload = JSON.parse(base64UrlDecode(encodedPayload));
+    
+    const validSignature = sign(`${encodedHeader}.${encodedPayload}`, secret);
+    if (validSignature !== signature) {
+      throw new Error('Invalid signature');
+    }
+  
+    return { header, payload };
+  }
+
 function sign(str, secret) {
   const hash = CryptoJS.HmacSHA256(str, secret);
   return base64UrlEncode(CryptoJS.enc.Base64.stringify(hash));
@@ -34,4 +56,5 @@ function sign(str, secret) {
 
 module.exports = {
   jessEncode,
+  jessDecode,
 };
